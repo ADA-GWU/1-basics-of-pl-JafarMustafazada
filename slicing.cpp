63d4ace6ec6aaec36e9a80b87fedcf944b6e6bdb
JafarMustafazada <154835144+JafarMustafazada@users.noreply.github.com>
2024-10-11 16:46:05 +0400

    Update slicing.cpp added image creating function

--- slicing.cpp
+++ slicing.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -46,12 +47,33 @@ void printMatrix(const vector<vector<int>>& matrix) {
 	cout << "]";
 }
 
+void createImage(const vector<vector<int>>& matrix) {
+	string temp = "<style>table, th, td {  border:1px solid black;}</style><table style=\"width:100 % \">";
+	for (int i = 0; i < matrix.size(); i++)
+	{
+		vector<int> row = matrix[i];
+		temp += "<tr>";
+		for (int j = 0; j < row.size(); j++)
+		{
+			temp += "<td>" + to_string(row[j]) + "</td>";
+		}
+		temp += "</tr>";
+	}
+	temp += "</table>";
+	
+	ofstream file1("result.html");
+	if (file1.is_open()) {
+		file1 << temp << std::endl;
+		file1.close();
+	}
+}
+
 int main() {
 	vector<vector<int>> matrix = {
-		{1, 2, 3, 10},
-		{4, 5, 6, 11},
-		{7, 8, 9, 12},
-		{13, 14, 15, 16},
+	{1, 2, 3, 10},
+	{4, 5, 6, 11},
+	{7, 8, 9, 12},
+	{13, 14, 15, 16},
 	};
 
 	printMatrix(matrix);
@@ -59,6 +81,8 @@ int main() {
 
 	vector<vector<int>> submatrix = matrixSlicing(matrix, 1, 1, -1, -1);
 	printMatrix(submatrix);
+	createImage(submatrix);
+	cout << "done";
 
 
 	return 0;
